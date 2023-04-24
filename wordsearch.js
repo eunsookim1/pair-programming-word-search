6278be775d7389594656c187f9f7619de57bee32
eunsookim1 <92eunsoo@gmail.com>
2023-04-24 14:23:44 +0900

    for of loop

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
 
-module.exports = wordSearch
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+module.exports = wordSearch;
+
+// function receives a 2D array of letters and a word
+// The function must:
+// check to find the word horizontally and vertically
+// return true if the word is found, and return false if the word is not found
