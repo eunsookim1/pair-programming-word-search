eb29aa5fe62bacf0378ef51eed4c3d67f76c01ef
eunsookim1 <92eunsoo@gmail.com>
2023-04-25 11:42:27 +0900

    final commit

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,7 +4,13 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the word (nothing) is not present", function() {
+    const result = wordSearch([], 'FRANK');
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word (FRANK) is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +26,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return false if the word is not present", function() {
+  it("should return true if the word (SEINFELD) is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -36,13 +42,36 @@ describe("#wordSearch()", function() {
     assert.isTrue(result);
   });
 
-  it("should return false if the word is present", function() {
-    const result = wordSearch([], 'SEINFELD');
+  it("should return true if the word (RENE) is present", function() {
+    const result = wordSearch([['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],], 'RENE');
+
+    assert.isTrue(result);
+  });
+
+  
+  it("should return false if the word (WENDY) is not present", function() {
+    const result = wordSearch([['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],], 'WENDY');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word (LARRY) is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -53,9 +82,56 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK');
+    ], 'LARRY'); // vertical
 
     assert.isTrue(result);
   });
 
+  it("should return true if the word (VET) is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'VET'); // backwards
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word (FAW) is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FAW'); // backwards
+
+    assert.isTrue(result);
+  });
+
+  // it("should return true if the word (AECT) is present", function() {
+  //   const result = wordSearch([
+  //     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  //     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+  //     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  //     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+  //     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+  //     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  //     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  //     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  //     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  //   ], 'AECT'); // diagonal
+
+  //   assert.isTrue(result);
+  // });
 });
